4dfcfef75576c6ed7f6a1c7c46665a3d69e60d06
carolineanjos <carolineanjoss02@gmail.com>
2024-06-01 08:16:06 -0300

    preparing files for new project

---
+++ src/api.jsx
@@ -0,0 +1,39 @@
+const API_KEY = "860a4477d6c95bca17367dd503797769";
+
+const categories = [
+    {
+        name: "trending",
+        title: "Em alta",
+        path: `/trending/movie/day?api_key=${API_KEY}&language=en-US`,
+    },
+    {
+        name: "netflixOriginals",
+        title: "Originais Netflix",
+        path: `/discover/movie?api_key=${API_KEY}`,
+    },
+    {
+        name: "TopRated",
+        title: "Populares",
+        path: `//movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    },
+    {
+        name: "TopRated",
+        title: "Populares",
+        path: `/movie/day?api_key=${API_KEY}&language=en-US`,
+    },
+    {
+        name: "comedy",
+        title: "Comédias",
+        path: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+    },
+    {
+        name: "romances",
+        title: "Romances",
+        path: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    },
+    {
+        name: "crimes",
+        title: "Crimes",
+        path: `discover/movie?api_key=${API_KEY}&with_genres=80`,
+    }
+]

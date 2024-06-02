6e760a66d3b350d39d11541859d0c61267f1bab6
carolineanjos <carolineanjoss02@gmail.com>
2024-06-02 07:10:24 -0300

    adding trailer to trending movies

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,12 +1,31 @@
+import movieTrailer from "movie-trailer";
+import ReactPlayer from "react-player";
 import { useEffect, useState } from "react";
 import { getMovies } from "../../api";
-
 import './styles.css';
+
 
 const imageHost = "https://image.tmdb.org/t/p/original/";
 
 export const Row = ({ title, path, isLarge }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+
+  const handleOnClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          setTrailerUrl(url);
+        })
+        .catch((error) => {
+          console.log("errorMovieTrailer", error);
+        });
+    }
+
+  };
 
   const fetchMovies = async (_path) => {
     try {
@@ -31,12 +50,14 @@ export const Row = ({ title, path, isLarge }) => {
           return (
             <img
               className={!isLarge ? "movie-card" : "movie-card-large"}
+              onClick={() => handleOnClick(movie)}
               key={movie.id}
               src={`${imageHost}${isLarge ? movie.backdrop_path : movie.poster_path}`}
               alt={movie.title} />
           );
         })}
       </div>
+      {trailerUrl && <ReactPlayer url={trailerUrl} playing={true} />}
     </div>
   );
 };

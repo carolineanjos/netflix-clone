26d67f8dd7616563c9371a70b81412d4226382f3
carolineanjos <carolineanjoss02@gmail.com>
2024-06-07 07:51:31 -0300

    adding scroll to the rows and fixing css

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,8 +1,9 @@
 import movieTrailer from "movie-trailer";
 import ReactPlayer from "react-player";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getMovies } from "../../api";
 import { FixedSizeList as List } from "react-window";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import './styles.css';
 
 
@@ -11,6 +12,13 @@ const imageHost = "https://image.tmdb.org/t/p/original/";
 export const Row = ({ title, path, isLarge }) => {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [isLeftDisabled, setIsLeftDisabled] = useState(true);
+  const [isRightDisabled, setIsRightDisabled] = useState(false);
+  const ref = useRef();
+
+  const movieCardWidth = isLarge ? 416 : 110;
+  const listWidth = 1470;
 
 
   const handleOnClick = (movie) => {
@@ -54,20 +62,72 @@ export const Row = ({ title, path, isLarge }) => {
     </div>
   );
 
+
+  const maxScrollPosition = (movies.length * movieCardWidth) - listWidth;
+
+  const toScroll = (scrollOffset) => {
+    const newScrollPosition = scrollPosition + scrollOffset;
+
+    if (newScrollPosition < 0) {
+      setScrollPosition(0);
+      ref.current.scrollTo(0);
+
+      setIsLeftDisabled(true);
+      setIsRightDisabled(false);
+
+    } else if (newScrollPosition > maxScrollPosition) {
+      setScrollPosition(maxScrollPosition);
+      ref.current.scrollTo(maxScrollPosition);
+
+      setIsLeftDisabled(false);
+      setIsRightDisabled(true);
+
+    } else {
+      setScrollPosition(newScrollPosition);
+      ref.current.scrollTo(newScrollPosition);
+
+      setIsLeftDisabled(false);
+      setIsRightDisabled(false);
+    }
+
+
+  };
+
   return (
     <div className="row-container">
       <h2 className="row-header">{title}</h2>
       <div className="row-cards">
+        {!isLeftDisabled &&
+          <button
+            onClick={() => toScroll(-1000)}
+            className={isLarge ? "arrow-left-button-large-cards" : "arrow-left-button-cards"}
+          >
+            <MdKeyboardArrowLeft size={isLarge ? 90 : 70} />
+          </button>
+        }
         <List
           className="list-container"
-          height={isLarge ? 300 : 180}
+          height={isLarge ? 300 : 200}
           itemCount={movies?.length}
-          itemSize={isLarge ? 416 : 100}
+          itemSize={movieCardWidth}
           layout="horizontal"
-          width={1400}
+          width={listWidth}
+          ref={ref}
+          scrollToItem={scrollPosition / listWidth}
         >
           {Column}
         </List>
+        {!isRightDisabled &&
+          <button
+            onClick={() => toScroll(1000)}
+            className={isLarge ?
+              ("arrow-left-button-large-cards" && "arrow-right-button-large-cards") :
+              ("arrow-left-button-cards" && "arrow-right-button-cards")}
+
+          >
+            <MdKeyboardArrowRight size={isLarge ? 90 : 70} />
+          </button>
+        }
       </div>
       {trailerUrl && <ReactPlayer url={trailerUrl} playing={true} />}
     </div>
